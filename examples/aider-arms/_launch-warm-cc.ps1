05ee97225f4f84e625493f3a8d1c83119c9a8e59
David <David@C0deGeek.dev>
2026-08-30 15:58:38 +0200

    Exclude retired operator experiments from distribution

--- examples/aider-arms/_launch-warm-cc.ps1
+++
@@ -1,3 +0,0 @@
-$env:JAVA_HOME = 'C:\Program Files\Eclipse Adoptium\jdk-21.0.11.10-hotspot'
-$env:PATH = "$env:JAVA_HOME\bin;$env:PATH"
-& 'C:\Users\David\.localbench\aider-arms.ps1' -Phase solve -Languages go,cpp,rust,javascript,java -Arms warm,claude-code -Limit 0 -RunTag '-v1' -EvalTimeout 900
--- examples/aider-arms/keepawake.ps1
+++
@@ -1,2 +0,0 @@
-Add-Type -Namespace W -Name P -MemberDefinition '[DllImport("kernel32.dll")] public static extern uint SetThreadExecutionState(uint e);'
-while($true){ [void][W.P]::SetThreadExecutionState([uint32]2147483649); Start-Sleep 240 }
